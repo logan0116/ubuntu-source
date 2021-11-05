bf6a623ae2ee2e6bcf45c904d025d0abfc01c10d
Logan <33882477+logan0116@users.noreply.github.com>
2021-11-05 15:53:42 +0800

    Add files via upload

---
+++ install-add.sh
@@ -0,0 +1,3 @@
+wget https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.1.32/11.3_06072021/cudnn-11.3-linux-x64-v8.2.1.32.tgz
+wget https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.1.32/11.3_06072021/Ubuntu20_04-x64/libcudnn8_8.2.1.32-1+cuda11.3_amd64.deb
+wget https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.1.32/11.3_06072021/Ubuntu20_04-x64/libcudnn8-dev_8.2.1.32-1+cuda11.3_amd64.deb
