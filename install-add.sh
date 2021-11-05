2579a22650a05ed3e961ac187f1dfc1a304e1d9e
Logan <33882477+logan0116@users.noreply.github.com>
2021-11-06 07:49:59 +0800

    Update install-add.sh

--- install-add.sh
+++ install-add.sh
@@ -1,3 +1,1 @@
-wget https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.1.32/11.3_06072021/cudnn-11.3-linux-x64-v8.2.1.32.tgz
-wget https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.1.32/11.3_06072021/Ubuntu20_04-x64/libcudnn8_8.2.1.32-1+cuda11.3_amd64.deb
-wget https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.1.32/11.3_06072021/Ubuntu20_04-x64/libcudnn8-dev_8.2.1.32-1+cuda11.3_amd64.deb
+wget https://gzc-download.weiyun.com/ftn_handler/bec0a22952a2bb3444fd9e18ca484b53bf371cf7aee53e4fee9fc280a1e75f3bdd5fe5d89b4e4ba69f64cc4fc6d22684ef24f4d20837ce682002e04371c0b107/nccl-local-repo-ubuntu2004-2.9.8-cuda11.3_1.0-1_amd64.deb?fname=nccl-local-repo-ubuntu2004-2.9.8-cuda11.3_1.0-1_amd64.deb&from=30113&version=3.3.3.3
