8be0e146c46ad65e44e9896263bd36c8b4140b15
Logan <33882477+logan0116@users.noreply.github.com>
2021-11-08 17:30:10 +0800

    Update install-add.sh

--- install-add.sh
+++ install-add.sh
@@ -1,1 +1,7 @@
-wget https://gzc-download.weiyun.com/ftn_handler/bec0a22952a2bb3444fd9e18ca484b53bf371cf7aee53e4fee9fc280a1e75f3bdd5fe5d89b4e4ba69f64cc4fc6d22684ef24f4d20837ce682002e04371c0b107/nccl-local-repo-ubuntu2004-2.9.8-cuda11.3_1.0-1_amd64.deb?fname=nccl-local-repo-ubuntu2004-2.9.8-cuda11.3_1.0-1_amd64.deb&from=30113&version=3.3.3.3
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.3.1/local_installers/cuda-repo-ubuntu2004-11-3-local_11.3.1-465.19.01-1_amd64.deb
+dpkg -i cuda-repo-ubuntu2004-11-3-local_11.3.1-465.19.01-1_amd64.deb
+apt-key add /var/cuda-repo-ubuntu2004-11-3-local/7fa2af80.pub
+apt-get update
+apt-get -y install cuda
