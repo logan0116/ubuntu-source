625555abafc0f4bc2cea6fcce5cd663152c4821b
Logan <33882477+logan0116@users.noreply.github.com>
2021-11-08 21:03:50 +0800

    Update install-add.sh

--- install-add.sh
+++ install-add.sh
@@ -1,1 +1,8 @@
-HOROVOD_GPU_OPERATIONS=NCCL pip3 install --no-cache-dir horovod
+curl https://get.docker.com | sh \
+  && sudo systemctl --now enable docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+apt-get update
+apt-get install -y nvidia-docker2
+systemctl restart docker
