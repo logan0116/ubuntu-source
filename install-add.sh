725cb86b2b6e42f821c96f4a18b9bbce3fa5772f
Logan <33882477+logan0116@users.noreply.github.com>
2021-11-08 20:46:48 +0800

    Update install-add.sh

--- install-add.sh
+++ install-add.sh
@@ -1,6 +1,1 @@
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-apt-get update
-apt install libnccl2=2.9.9-1+cuda11.3 libnccl-dev=2.9.9-1+cuda11.3
+HOROVOD_GPU_OPERATIONS=NCCL pip3 install --no-cache-dir horovod
