f02a34fe017df8db058c5e869c8c330101600a2a
Logan <33882477+logan0116@users.noreply.github.com>
2021-11-09 15:53:17 +0800

    Create docker-start.sh

---
+++ docker-start.sh
@@ -0,0 +1,8 @@
+apt update
+apt upgrade -y
+cp -rf /usr/local/source.list /etc/apt/sources.list
+apt update
+apt upgrade -y
+apt install gcc automake autoconf libtool make wget openssl libssl-dev software-properties-common vim cmake curl python3 python3-pip -y
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
+pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
