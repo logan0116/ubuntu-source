14600e1acce834bea0f73dbfb124c8fd6b0010e0
Logan <33882477+logan0116@users.noreply.github.com>
2021-11-09 14:28:21 +0800

    Update install-add.sh

--- install-add.sh
+++ install-add.sh
@@ -1,8 +1,2 @@
-curl https://get.docker.com | sh \
-  && sudo systemctl --now enable docker
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-apt-get update
-apt-get install -y nvidia-docker2
-systemctl restart docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
